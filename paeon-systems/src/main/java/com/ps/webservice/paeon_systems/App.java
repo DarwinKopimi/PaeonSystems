ff2ac54d0dce6bf38011805d57caffd78ed3a2ec
DarwiniKopimi <nickablair@gmail.com>
2018-08-31 23:38:29 -0400

    Initial project commit for Maven application and git. Signed-off-by: Nick <waldo@waldo-MS-7A34>

---
+++ paeon-systems/src/main/java/com/ps/webservice/paeon_systems/App.java
@@ -0,0 +1,13 @@
+package com.ps.webservice.paeon_systems;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
